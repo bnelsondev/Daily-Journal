2a1aff3c4c116f5c63e6b22716e70170c376516c
Bryan Nelson <bryananelsonjr@gmail.com>
2021-01-22 11:26:49 -0500

    Custom Cell

--- Daily Journal/JournalTableViewController.swift
+++ Daily Journal/JournalTableViewController.swift
@@ -10,10 +10,10 @@ import UIKit
 class JournalTableViewController: UITableViewController {
     
     var entries: [Entry] = []
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -24,28 +24,33 @@ class JournalTableViewController: UITableViewController {
             }
         }
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return entries.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
-        let cell = UITableViewCell()
-
-        let entry = entries[indexPath.row]
-        
-        cell.textLabel?.text = entry.text
-
-        return cell
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "EntryCell") {
+            
+            let entry = entries[indexPath.row]
+            
+            //cell.textLabel?.text = entry.text
+            
+            return cell
+        } else {
+            return UITableViewCell()
+        }
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let entry = entries[indexPath.row]
         performSegue(withIdentifier: "segueToEntry", sender: entry)
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
