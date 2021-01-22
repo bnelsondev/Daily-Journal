1c4451dd10db7ce65a9273af22f6673d1082d4a5
Bryan Nelson <bryananelsonjr@gmail.com>
2021-01-22 01:28:38 -0500

    Added CoreData

---
+++ Daily Journal/Entry+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Entry+CoreDataClass.swift
+//  Daily Journal
+//
+//  Created by Bryan Nelson on 1/22/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Entry)
+public class Entry: NSManagedObject {
+
+}
--- Daily Journal/EntryViewController.swift
+++ Daily Journal/EntryViewController.swift
@@ -8,22 +8,25 @@
 import UIKit
 
 class EntryViewController: UIViewController {
-
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var entryTextView: UITextView!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        entryTextView.isScrollEnabled = false
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillDisappear(_ animated: Bool) {
+        // Make an entry
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            let entry = Entry(context: context)
+            entry.date = datePicker.date
+            entry.text = entryTextView.text
+            (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+        }
     }
-    */
-
 }
